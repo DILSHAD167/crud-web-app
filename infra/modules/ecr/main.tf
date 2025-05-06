2b1aa41d1a9353292b1f75c8b8dda90b79f3b114
DILSHAD167 <dilshadaws167@gmail.com>
2025-05-06 12:57:00 +0530

    Update main.tf

--- infra/modules/ecr/main.tf
+++ infra/modules/ecr/main.tf
@@ -1,6 +1,7 @@
     resource "aws_ecr_repository" "example" {
       name                  = var.ecr_name # Replace with your desired repository name
       image_tag_mutability = "MUTABLE" # Or "IMMUTABLE"
+      force delete = true
       image_scanning_configuration {
         scan_on_push = false
       }
